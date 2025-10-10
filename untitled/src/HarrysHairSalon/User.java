8a2a777efde3524f0b7926188bb999d1d8226826
DrOfori <simonbelai045@gmail.com>
2025-10-10 12:51:24 +0200

    tilføjet setters

--- untitled/src/HarrysHairSalon/User.java
+++ untitled/src/HarrysHairSalon/User.java
@@ -1,4 +1,38 @@
 package HarrysHairSalon;
 
-public class User {
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected UserRole role;
+
+    public User(String username, String password, UserRole role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {return username;}
+    public String getPassword() {return password;}
+    public String getRole() {return role;}
+
+    public void setUsername(String username) {this.username = username;}
+    public void setPassword(String password) {this.password = password;}
+    public void setRole(UserRole role) {this.role = role;}
+
+    public String getUserInfo(){
+        return username + password + role;
+    }
+
+    @Override
+    public String toString(){
+        return getUserInfo();
+    }
+
+    public boolean login (String password){
+        return this.password.equals(password);
+    }
+
+    public abstract Permission getPermission();
+
+    public abstract boolean canAccessFinacials();
 }
