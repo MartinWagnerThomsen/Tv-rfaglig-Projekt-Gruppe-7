30fa0e8889c6872bf6ea6235bb552e50094ac7ed
DrOfori <simonbelai045@gmail.com>
2025-10-10 13:46:42 +0200

    UserRole

--- untitled/src/HarrysHairSalon/User.java
+++ untitled/src/HarrysHairSalon/User.java
@@ -13,7 +13,7 @@ public abstract class User {
 
     public String getUsername() {return username;}
     public String getPassword() {return password;}
-    public String getRole() {return role;}
+    public UserRole getRole() {return role;}
 
     public void setUsername(String username) {this.username = username;}
     public void setPassword(String password) {this.password = password;}
