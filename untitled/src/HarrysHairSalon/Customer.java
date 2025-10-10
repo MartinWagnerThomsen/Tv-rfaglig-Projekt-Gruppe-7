0be4c80691682586209928c8b2369929e5f1e1d8
DrOfori <simonbelai045@gmail.com>
2025-10-10 11:42:03 +0200

    Konstruktør, getters, setters og toString

--- untitled/src/HarrysHairSalon/Customer.java
+++ untitled/src/HarrysHairSalon/Customer.java
@@ -1,4 +1,49 @@
 package HarrysHairSalon;
 
 public class Customer {
+    private int id;
+    private String name;
+    private String phone;
+    private String mail;
+    private boolean hasCredit;
+
+    public Customer (int id, String name, String phone, String mail, boolean hasCredit){
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.mail = mail;
+        this.hasCredit = false;
+    }
+
+//Getters
+    public int getId(){return id;}
+
+    public String getName(){return name;}
+
+    public String getPhone(){return phone;}
+
+    public String getmail (){return mail;}
+
+    public boolean hasCredit(){return hasCredit;}
+
+//Setters
+    public void setId(int id){this.id = id;}
+
+    public void setName(String name){this.name = name;}
+
+    public void setPhone(String phone){this.phone = phone;}
+
+    public void setMail(String mail){this.mail = mail;}
+
+    public void setHasCredit(boolean hasCredit){this.hasCredit = hasCredit;}
+
+
+public String CustomerInfo(){
+    return new String ("Customer Info:\n" + "ID: " + id + "\nName: " + name +
+            "\nPhone: " + phone + "\nMail: " + mail + "\nCredit: " + hasCredit);
+    }
+    
+@Override
+public String toString(){
+    return CustomerInfo();
 }
