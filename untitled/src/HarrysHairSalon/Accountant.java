4ffc77294a7fca8dc992ffc9d952435142cf533c
jacobvad1337 <jacobvad@Aiwa.local>
2025-10-11 19:01:42 +0200

    Lavet hairdresser og accountant + kommentere til de enkelte ting

--- untitled/src/HarrysHairSalon/Accountant.java
+++ untitled/src/HarrysHairSalon/Accountant.java
@@ -1,4 +1,36 @@
 package HarrysHairSalon;
 
-public class Accountant {
+public class Accountant extends User {
+
+    //Laver username for accountant, og trækker rollen fra vores UserRole klasse
+    public Accountant(String username, UserRole role){
+        super(username, role);
+    }
+    //Fortæller at accountant kun kan læse i systemet og har får muligheden for at se dagens revenue + generer en rapport
+    @Override
+    public String getPermissions(){
+        return "READ ONLY CAN SEE DAILY REVENUE OR GENERATE REPORT";
+    }
+    //Fortæller at accountant får adgang til financials
+    @Override
+    public boolean canAccesFinancials(){
+        return true;
+    }
+    //Fortæller accountant at de ikke kan lave nye aftaler
+    @Override
+    public void createAppointment(String appointments){
+        System.out.println(username + " " + "CAN'T CREATE APPOINTMENT");
+    }
+    //Fortæller at accountant ikke kan slette nogen aftaler
+    @Override
+    public void deleteAppointment(String appointments){
+        System.out.println(username + " " + "CAN'T DELETE APPOINTMENT");
+    }
+    //Laver print text på at den generer en rapport
+    public void generateFinancialReport(){
+        System.out.println(username + " " + "GENERATING FINANCIAL REPORT....");
+    }
+    public void viewDailyRevenue(){
+        System.out.println(username + " " + "HERE IS A VIEW OF THE DAILY REVENUE");
+    }
 }
--- untitled/src/HarrysHairSalon/Hairdresser.java
+++ untitled/src/HarrysHairSalon/Hairdresser.java
@@ -1,4 +1,35 @@
 package HarrysHairSalon;
 
-public class Hairdresser {
+// Laver kodeordet "hairyharry"
+public class Hairdresser extends User {
+    private String password = "hairyharry";
+
+    // Bruger vores superklasse til at få username, rolle og password
+    public Hairdresser(String username, UserRole role) {
+        super(username, password, role);
+    }
+// Fortæller at man kan logge ind hvis password er det rigtige password
+    public boolean logIn(String password) {
+        return this.password.equals(password);
+    }
+// Man kan få lov til lave eller slette aftaler
+    @Override
+    public String getPermission() {
+        return "Can choose to create or delete appointments";
+    }
+// At man får adgang til financials
+    @Override
+    public boolean canAccesFinancials() {
+        return true;
+    }
+// Man kan lave aftaler, og får et output på hvilken aftale man har lavet
+    @Override
+    public void createAppointment(String appointments) {
+        System.out.println(username + " " + "creates appointment" + appointments);
+    }
+// Man kan slette aftaler, og får et output på hvilken aftale man så har slettet
+    @Override
+    public void deleteAppointment(String appointments) {
+        System.out.println(username + " " + "deletes appointment" + appointments);
+    }
 }
