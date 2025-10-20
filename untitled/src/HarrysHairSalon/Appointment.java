b06942e6a57e55efff16b984eb22be3a10e53b77
jacobvad1337 <jacobvad@Aiwa.local>
2025-10-20 14:09:40 +0200

    Appointment lavet som udgangspunkt

--- untitled/src/HarrysHairSalon/Appointment.java
+++ untitled/src/HarrysHairSalon/Appointment.java
@@ -1,4 +1,72 @@
 package HarrysHairSalon;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Appointment {
+    private int id;
+    private Customer customer;
+    private LocalDateTime dateTime;
+    private int duration;
+    private Order order;
+    private AppointmentStatus status;
+
+    //Konstruktøre
+    public Appointment(int id, Customer customer, LocalDateTime dateTime, int duration, Order order){
+        this.id = id;
+        this.customer = customer:
+        this.dateTime = dateTime;
+        this.duration = duration;
+        this.order = order;
+        this.status = AppointmentStatus.SCHEDULED;
+    }
+
+    //Gettere
+    public int getID() {return id; }
+    public Customer getCustomer() { return customer; }
+    public LocalDateTime getDateTime() { return dateTime; }
+    public int getDuration() { return duration; }
+    public Order getOrder() { return order; }
+    public AppointmentStatus getStatus() { return status; }
+
+    //Laver detajlerne for aftalen
+    public String getAppointmentDetails() {
+        return "Appointment ID: " + id +
+                "\nCustomer: " + customer.getName() +
+                "\nDate and time: " dateTime +
+                "\nDuration in minutes: " + duration +
+                "\nOrder: " + order.getOrderDetails() +
+                "\nStauts: " + status;
+    }
+
+    //Fuldføre aftalen
+    public void completeAppointment(){
+        if (status != AppointmentStatus.SCHEDULED) {
+            status = AppointmentStatus.COMPLETED;
+            System.out.println("Appointment " + id + " is booked");
+        } else {
+            System.out.println("Appointment has been cancelled");
+        }
+    }
+
+    //Aflys aftalerne
+    public void cancelAppointment(){
+        if (status == AppointmentStatus.COMPLETED) {
+            System.out.println("Cannot cancel appointment");
+        } else {
+            status = AppointmentStatus.CANCELLED;
+            System.out.println("Appointment has been cancelled");
+
+        }
+    }
+
+    //Find ud af om din booking er gennemført
+    public boolean isCompleted(){
+        return status == AppointmentStatus.COMPLETED;
+    }
+
+    @Override
+    public String toString(){
+        return getAppointmentDetails();
+    }
 }
