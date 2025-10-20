ef396399bc2f289d42a70fb5a06ab46cb8e1835e
DrOfori <simonbelai045@gmail.com>
2025-10-20 13:52:25 +0200

    Order class

--- untitled/src/HarrysHairSalon/Order.java
+++ untitled/src/HarrysHairSalon/Order.java
@@ -1,4 +1,56 @@
 package HarrysHairSalon;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Order {
+    private int orderId;
+    private double haircutPrice;
+    private List<Product> products;
+    private Payment payment;
+    private double totalAmount;
+    private LocalDate orderDate;
+    private LocalTime orderTime;
+
+
+    public Order(int orderId, double haircutPrice){
+        this.orderId = orderId;
+        this.haircutPrice = haircutPrice;
+        this.products = new ArrayList<>();
+        this.totalAmount = haircutPrice;
+        this.orderDate = LocalDate.now();
+        this.orderTime = LocalTime.now();
+    }
+
+    public int getOrderId(){return orderId;}
+    public List<Product> getProducts() {return products;}
+    public double getTotalAmount() {return totalAmount;}
+
+    public void setPayment(Payment payment) {this.payment = payment;}
+
+
+    public void addProduct(Product product){
+        products.add(product);
+        totalAmount += product.getPrice();
+    }
+
+    public void calculateTotal(){
+        totalAmount = haircutPrice;
+        for (Product p : products) {
+            totalAmount += p.getPrice();
+        }
+    }
+
+    public void processPayment() {
+        if (payment != null){
+            payment.markAsPaid();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Order #" + orderId + "\nAmount: " + totalAmount + " DKK";
+    }
 }
