db342047f88ed615123dac0c601782a1050de5c1
jacobvad1337 <jacobvad@Aiwa.local>
2025-10-22 09:09:15 +0200

    rettet så det ser lidt pænere ud

--- untitled/src/HarrysHairSalon/Hairdresser.java
+++ untitled/src/HarrysHairSalon/Hairdresser.java
@@ -7,32 +7,44 @@ public class Hairdresser extends User {
     public Hairdresser(String username, UserRole role) {
         // Laver hairyharry som password
         super(username, "hairyharry", role);
+
     }
+
 // Man kan få lov til lave eller slette aftaler
     @Override
     public String getPermission() {
         return "FULL ACCESS: Can choose to create or delete appointments";
+
     }
+
 // At man får adgang til financials
     @Override
     public boolean canAccessFinancials() {
         return true;
+
     }
+
 // Man kan lave aftaler, og får et output på hvilken aftale man har lavet
     @Override
     public void createAppointment(String appointments) {
         System.out.println(username + " " + "creates appointment" + appointments);
+
     }
+
 // Man kan slette aftaler, og får et output på hvilken aftale man så har slettet
     @Override
     public void deleteAppointment(String appointments) {
         System.out.println(username + " " + "deleted appointment" + appointments);
+
     }
+
     // Harry skal også kunne generere report
     @Override
     public void generateFinancialReport(){
         System.out.println(username + " " + "is generating report...");
+
     }
+
     // Harry skal også have en oversigt over dagens revenue
     @Override
     public void viewDailyRevenue(){
